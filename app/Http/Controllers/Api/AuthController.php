e0a47003abac45aecd8169f15804ec8dffd84e36
MatheusPlinio <mplinio13@gmail.com>
2025-10-19 14:26:45 -0300

    stash

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\StoreAuthRequest;
+
+class AuthController extends Controller
+{
+    public function register(StoreAuthRequest $request)
+    {
+        
+    }
+}
